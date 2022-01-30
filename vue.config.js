0f1a07d46186efbcd0440e323e57cafc3f001d7b
mishajib <mishajib222@gmail.com>
2022-01-31 00:11:10 +0600

    deploy script added

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath : process.env.NODE_ENV === 'production'
-                 ? '/vue-firebase-app/'
-                 : '/'
+    publicPath : process.env.NODE_ENV === 'production' ? '/vue-firebase-crud/' : '/'
 }
